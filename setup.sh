19b2df0529366d806210e942719966f9a79c72db
szeyuilau <szeyuihk25@gmail.com>
2020-11-02 17:40:00 +0800

    first commit

---
+++ setup.sh
@@ -0,0 +1,10 @@
+mkdir -p ~/.streamlit/
+echo "\
+[general]\n\
+email = \"szeyuihk25@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
